37596897f63d25eea01ffcaff9e6f43e5c851b00
Gilmer Contreras <adgdiux@gmail.com>
2022-05-01 12:48:53 -0500

    Correcciones en Productos

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 app.use('/api/clients', require('./routes/clients.route'));
 app.use('/api/login', require('./routes/auth.route'));
 app.use('/api/products', require('./routes/products.route'));
+app.use('/api/preventives', require('./routes/preventives.route'));
 app.use('/api/users', require('./routes/users.route'));
 
 // SPA
